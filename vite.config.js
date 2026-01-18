eb6791aa544d7f2738946b65b3b7fa50f0cc2237
ILBOUDOChristian <ilboudochris79@gmail.com>
2026-01-18 01:36:16 +0000

    correction du chemin vite

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/brandbrain-ai/', // Remplacez par le nom EXACT de votre dépôt sur GitHub
+    base: '/ia.assistant/', // Doit correspondre exactement au nom de votre dépôt GitHub
 })
